3e83ddd0939e54ddc2e2808c8cf26c4eb14adde9
David Cavalcanti <david_cavalcanti@outlook.com>
2023-04-06 17:08:28 -0300

    Adicionando arquivo de locators para o a aplicação ALURAPIC

---
+++ cypress/support/locatorsAlura.js
@@ -0,0 +1,7 @@
+export const locatorsAlura = {
+    LOGIN: {
+        USER: 'input[formcontrolname="userName"]',
+        PASSWORD: 'input[formcontrolname="password"]',
+        BUTTON_LOGIN: 'button[type="submit"]'
+    }
+}
