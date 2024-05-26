1177ec853da593c2b94e104d19c1c6f392e43bc5
David Cavalcanti <david.cavalcanti@i.systems.com.br>
2024-05-26 15:58:05 -0300

    add environment variables

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    standard_user: 'standard_user', 
+    locked_user: 'locked_out_user',
+    password: 'secret_sauce',
+    urlLogged: 'https://www.saucedemo.com/inventory.html'
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
